6b407634bdcd1e6be78d1704100b8c7d046ffe40
Aayush <ayush2991@gmail.com>
2026-07-10 15:39:56 -0400

    Initial Commit

---
+++ caliber/caliberApp.swift
@@ -0,0 +1,17 @@
+//
+//  caliberApp.swift
+//  caliber
+//
+//  Created by Aayush Agarwal on 7/10/26.
+//
+
+import SwiftUI
+
+@main
+struct caliberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ caliberTests/caliberTests.swift
@@ -0,0 +1,19 @@
+//
+//  caliberTests.swift
+//  caliberTests
+//
+//  Created by Aayush Agarwal on 7/10/26.
+//
+
+import Testing
+@testable import caliber
+
+struct caliberTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
